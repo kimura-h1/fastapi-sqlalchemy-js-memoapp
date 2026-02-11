88e7321c049f384f5269d3af7aa9b5497a05a449
Kimura Hiromichi <hk8170@gmail.com>
2026-02-11 22:11:59 +0900

    不要なコメント削除。

--- frontapp-react/src/App.jsx
+++ frontapp-react/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CreateMemo from "./CreateMemo";
 import ListMemos from "./ListMemos";
+import EditMemo from "./EditMemo"; 
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <Route path="/" element={<Navigate to="/list" replace />} />
         <Route path="/list" element={<ListMemos />} />
         <Route path="/create" element={<CreateMemo />} />
+        <Route path="/memos/:id/edit" element={<EditMemo />} />
+         <Route path="*" element={<Navigate to="/list" replace />} />
       </Routes>
     </BrowserRouter>
   );
